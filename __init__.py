66d6dc3b83928a3107ad43e297fd3ca59f93a375
Scott B5 <busingescott@gmail.com>
2016-10-21 05:37:26 +0300

    commits

--- __init__.py
+++ __init__.py
@@ -1,19 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
-from flask_login import LoginManager
-from .auth import auth as auth_blueprint
 
 db = SQLAlchemy()
-
-"""
-Flask-Login provides user session management for Flask.
-It handles the common tasks of logging in, logging out,
-and remembering your users’ sessions over extended periods of time.
-"""
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -24,8 +13,5 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
     db.init_app(app)
-    login_manager.init_app(app)
     return app
---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    '''This base class contains configuration
+    that is common in all environments
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dolor ipsum Lorem'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopementConfig(Config):
+    '''This class configures the development
+    environment properties
+    '''
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'online_store.db')
+
+
+class ProductionConfig(Config):
+    '''This class cofigures the production
+    environment properties
+    '''
+    PORT = int(os.environ.get("PORT", 5000))
+    HOST = '0.0.0.0'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'online_store.db')
+
+
+config = {
+    'development': DevelopementConfig,
+    'production': ProductionConfig,
+    'default': DevelopementConfig
+}
